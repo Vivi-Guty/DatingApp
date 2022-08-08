efdefadceae87266b1d7555025ee3c38067c773d
Victor Gutierrez García <victor.gutierrez@inercya.com>
2022-08-08 14:54:17 +0200

    ha fallado

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -22,15 +21,4 @@ export class AppComponent implements OnInit {
             error: error => console.log(error)
         });
     }
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'client';
->>>>>>> fc16c54585148279ea0d1e64310a8585ba029c8c
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
->>>>>>> fc16c54585148279ea0d1e64310a8585ba029c8c
 
 @NgModule({
   declarations: [
@@ -18,13 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule
-=======
-    AppRoutingModule
->>>>>>> fc16c54585148279ea0d1e64310a8585ba029c8c
   ],
   providers: [],
   bootstrap: [AppComponent]
